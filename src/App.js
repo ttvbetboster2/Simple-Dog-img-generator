f56cff68f36dca166921991810bccd9b9dc8315c
Chitraksh Kataria <chitrakshkatariv3@gmail.com>
2025-04-07 10:13:30 +0200

    This is the simple Dog-img-gen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+
 function App() {
+  const [dogImage, setDogImage] = useState(''); //Storage to store the image uRL
+  const [animate, setAnimate] = useState(false);
+
+  const fetchDog = async () => {
+    try {
+      const response = await fetch('https://dog.ceo/api/breeds/image/random'); //get the img
+      const data = await response.json();
+
+      setAnimate(false);
+      setDogImage (data.message);
+      setTimeout(() => {
+        setAnimate(true);
+      }, 50);
+    } catch (err) {
+      console.error('Failed to fetch dog image:', err);
+    }
+  }; 
+
+  useEffect(() => {
+    fetchDog();
+  }, []); 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      {dogImage && (
+        <div
+          className={`dog-background ${animate ? 'fade-in' : ''}`}
+          style={{ backgroundImage: `url(${dogImage})` }}
+        />
+      )}
+      
+      <div className="centered-content">
+        <h1>🐶 Random Dog</h1>
+        <button onClick={fetchDog}>Fetch New Dog</button>
+      </div>
     </div>
   );
+  
 }
 
 export default App;
